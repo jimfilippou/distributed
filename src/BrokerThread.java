925b0e0d7d215e063c94d08168f1284b74a4a040
p3150127 <anna.noukou@gmail.com>
2019-04-04 17:42:13 +0300

    malakiaaaa

--- src/BrokerThread.java
+++ src/BrokerThread.java
@@ -4,7 +4,7 @@ import Models.Broker;
 import java.io.*;
 import java.net.*;
 
-public class BrokerThread implements Runnable {
+public class BrokerThread {
 
     private Broker broker;
     private Thread t;
@@ -15,68 +15,96 @@ public class BrokerThread implements Runnable {
 
     private void startSender() {
 
-        while (true) {
-            //Thread.sleep(500);
-            for (Broker broker : BrokerProvider.fetchBrokers()) {
-                if (broker.getIP().equals("172.16.2.21")) continue;
-                (new Thread(() -> {
-                    BufferedWriter out;
-                    try (Socket s = new Socket(broker.getIP(), broker.getPort())) {
-                        out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-                        out.write(broker.getHash());
-                        out.newLine();
-                        out.flush();
-                    } catch (ConnectException err) {
-                        // Connection failed because not all brokers on the txt are up
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                })).start();
+        for (Broker broker : BrokerProvider.fetchBrokers()) {
+            BufferedWriter out;
+            try (Socket s = new Socket(broker.getIP(), broker.getPort())) {
+                out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+                out.write(broker.getHash());
+                out.newLine();
+                out.flush();
+            } catch (ConnectException err) {
+                // Connection failed because not all brokers on the txt are up
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
         }
+
 
     }
 
     private void startServer() {
-        (new Thread(() -> {
-            ServerSocket providerSocket = null;
+
+        ServerSocket providerSocket = null;
+        BufferedReader in = null;
+        try {
+            InetAddress addr = InetAddress.getByName(this.broker.getIP());
+            providerSocket = new ServerSocket(this.broker.getPort(), 50, addr);
+            System.out.println("BrokerThread started at:" + addr + ":" + this.broker.getPort());
+
+
+            // create a new thread object
+            Thread t = new ClientHandler(providerSocket);
+
+            // Invoking the start() method
+            t.start();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    // ClientHandler class
+    class ClientHandler extends Thread {
+        final ServerSocket s;
+
+        // Constructor
+        public ClientHandler(ServerSocket s) {
+            this.s = s;
+        }
+
+        public void run() {
+
+            Socket s = null;
             try {
-                InetAddress addr = InetAddress.getByName(this.broker.getIP());
-                providerSocket = new ServerSocket(this.broker.getPort(), 50, addr);
-                System.out.println("BrokerThread started at:" + addr + ":" + this.broker.getPort());
-                Socket s = providerSocket.accept();
-                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                String line = null;
-                while ((line = in.readLine()) != null) {
-                    System.out.println(line);
-                }
+                s = this.s.accept();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        })).start();
-    }
+            while (true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
 
-    @Override
-    public void run() {
-        if (broker.getIP().equals("172.16.2.21")) {
-            System.out.println("Starting server...");
-            this.startServer();
-            System.out.println("Starting sender...");
-            this.startSender();
-        } else {
-            System.out.println("Starting sender...");
-            this.startSender();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    // receive the answer from client
+
+
+                    String line = null;
+                    while ((line = in.readLine()) != null) {
+                        System.out.println(line);
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
 
     }
+
 
     public void start() {
-        String threadname = "THREAD" + this.broker.getPort();
-        Thread thread = this.t;
-        if (thread == null) {
-            thread = new Thread(this, threadname);
-            thread.start();
-        }
+        System.out.println("Starting server...");
+        this.startServer();
+        System.out.println("Starting sender...");
+        this.startSender();
     }
 }
